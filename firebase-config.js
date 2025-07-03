0e75fdf7824f2c8d7acd1b99d757e4c041053aeb
Bench <bench5653@gmail.com>
2025-07-03 14:30:34 +0800

    up

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,11 @@
-// TODO: Add your Firebase project configuration here
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDTAjEgyFixXk8yFLpWqwXTnTZPeqlWN-Y",
   authDomain: "ecommerce-70f46.firebaseapp.com",
@@ -9,26 +16,6 @@ const firebaseConfig = {
   measurementId: "G-DJJLK89T9C"
 };
 
-// Initialize Firebase (this should ideally be done once)
-// We'll handle the actual initialization within each script
-// that needs it, checking if it's already initialized.
-
-// Export the config for use in other scripts
-// For browser environments, you might not use ES6 modules directly without a bundler.
-// Instead, scripts will access firebaseConfig globally or it will be passed around.
-// For simplicity here, we assume firebase-config.js is loaded first and
-// firebaseConfig becomes a global variable.
-
-console.log("Firebase config loaded (remember to replace placeholders).");
-
-// It's good practice to wrap Firebase SDK calls.
-// For now, we'll let auth.js and app.js initialize Firebase app and services themselves
-// after checking if an app instance already exists.
-// This avoids issues if scripts are loaded in an unexpected order or multiple times.
-
-// Example of how other files might use it:
-// if (typeof firebase !== 'undefined' && !firebase.apps.length) {
-// firebase.initializeApp(firebaseConfig);
-// }
-// const auth = firebase.auth();
-// const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
